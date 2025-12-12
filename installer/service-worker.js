cc87cbf287c3a9497c57537f96a78f87b964415e
blairavery13-png <blairavery13@gmail.com>
2025-12-12 22:00:02 +1000

    Update service worker cache version to v3

--- installer/service-worker.js
+++ installer/service-worker.js
@@ -1,22 +1,12 @@
-const VERSION="v2";
-const CACHE="installer-"+VERSION;
-
+const CACHE="installer-v3";
 self.addEventListener("install",e=>{
 self.skipWaiting();
-e.waitUntil(caches.open(CACHE).then(c=>c.addAll([
-"./",
-"./index.html",
-"./manifest.json"
-])));
+e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"])));
 });
-
 self.addEventListener("activate",e=>{
-e.waitUntil(
-caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))
-);
+e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));
 self.clients.claim();
 });
-
 self.addEventListener("fetch",e=>{
 e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
