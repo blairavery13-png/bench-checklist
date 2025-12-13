bd1883fa2789536ca6f9600d943daf0171539452
blairavery13-png <blairavery13@gmail.com>
2025-12-13 16:26:58 +1000

    Refactor service worker event listeners for clarity

--- installer/service-worker.js
+++ installer/service-worker.js
@@ -1,12 +1,27 @@
-const CACHE="installer-daily-v4";
-self.addEventListener("install",e=>{
-self.skipWaiting();
-e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"])));
+const CACHE = "installer-daily-v4";
+
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll([
+      "./",
+      "./index.html",
+      "./manifest.json"
+    ]))
+  );
 });
-self.addEventListener("activate",e=>{
-e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-self.clients.claim();
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
-self.addEventListener("fetch",e=>{
-e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
