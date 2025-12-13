4e062ee5d2d17ba3e8cdbc82d0de78e929d43124
blairavery13-png <blairavery13@gmail.com>
2025-12-14 07:12:41 +1000

    Update service worker to version v5

--- installer/service-worker.js
+++ installer/service-worker.js
@@ -1,27 +1,12 @@
-const CACHE = "installer-daily-v4";
-
-self.addEventListener("install", (e) => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE).then((c) => c.addAll([
-      "./",
-      "./index.html",
-      "./manifest.json"
-    ]))
-  );
+const CACHE="installer-daily-v5";
+self.addEventListener("install",e=>{
+self.skipWaiting();
+e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"])));
 });
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener("activate",e=>{
+e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));
+self.clients.claim();
 });
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+self.addEventListener("fetch",e=>{
+e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
